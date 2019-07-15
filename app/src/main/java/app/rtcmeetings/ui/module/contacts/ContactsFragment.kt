a4ca2d7b437eb35f0d4e548ad0137a062e3b4928
Serhii Yakovenko <serhii.yakovenko@coreteka.com>
2019-07-15 22:07:34 +0300

    call from contacts

--- app/src/main/java/app/rtcmeetings/ui/module/contacts/ContactsFragment.kt
+++ app/src/main/java/app/rtcmeetings/ui/module/contacts/ContactsFragment.kt
@@ -1,16 +1,26 @@
 package app.rtcmeetings.ui.module.contacts
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.rtcmeetings.R
 import app.rtcmeetings.base.BaseFragment
+import app.rtcmeetings.data.db.dbentity.Contact
+import app.rtcmeetings.data.db.dbentity.toUser
 import app.rtcmeetings.network.result.Status
+import app.rtcmeetings.network.ws.WsService
+import app.rtcmeetings.webrtc.CallEvent
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_contacts.*
 import javax.inject.Inject
@@ -19,6 +29,8 @@ class ContactsFragment : BaseFragment() {
 
     @Inject
     lateinit var viewModel: ContactsViewModel
+
+    private var wsService: WsService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +45,20 @@ class ContactsFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_contacts, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+        activity?.bindService(
+                Intent(context!!, WsService::class.java),
+                serviceConnection,
+                Context.BIND_AUTO_CREATE
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        activity?.unbindService(serviceConnection)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         observeData()
         viewModel.getContacts()
@@ -45,13 +71,21 @@ class ContactsFragment : BaseFragment() {
         Navigation.findNavController(view!!).navigateUp()
     }
 
+    private fun startCall(contact: Contact) {
+        wsService?.getSocketId()?.let { socketId ->
+            CallEvent.startCall(context!!, contact.toUser(), socketId)
+        } ?: Toast.makeText(context!!, "No WS connection", Toast.LENGTH_SHORT).show()
+    }
+
     private fun observeData() {
         viewModel.getContactsLiveData.observe(viewLifecycleOwner, Observer {
             when (it.status) {
                 Status.SUCCESS -> {
                     rvContacts.run {
                         layoutManager = LinearLayoutManager(context!!, RecyclerView.VERTICAL, false)
-                        adapter = ContactsAdapter(it.data)
+                        adapter = ContactsAdapter(it.data) { contact ->
+                            startCall(contact)
+                        }
                     }
                 }
                 Status.LOADING -> {
@@ -71,5 +105,14 @@ class ContactsFragment : BaseFragment() {
                 }
             }
         })
+    }
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            wsService = (service as WsService.LocalBinder).service
+        }
     }
 }
